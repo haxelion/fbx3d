31a1aff9d66c3f65e5fc088be8579131a7e13ddf
Charles Hubain <github@haxelion.eu>
2017-03-19 21:28:46 +0100

    Restructuring and a bit of documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
+//! The `fbx` crate allow to read `fbx` 3D model files. It is based on the `blender` implementation 
+//! of the `FBX` file format.
+
 extern crate bytepack;
 extern crate flate2;
 
-mod types;
+use std::io::{Read, Result, Error, ErrorKind, Seek};
+use std::u64;
+
+use bytepack::LEUnpacker;
+
+pub mod types;
+
+use types::{Node, decode_node_list};
+
+/// Decode a FBX file to a [`Node`](types/struct.Node.html) hierarchy.
+pub fn decode_fbx<R: Read + Seek>(r: &mut R) -> Result<Vec<Node>> {
+    let mut header = [0u8; 23];
+    r.read_exact(&mut header[..])?;
+    if &header != b"Kaydara FBX Binary  \x00\x1a\x00" {
+        return Err(Error::new(ErrorKind::InvalidData, "Invalid FBX header magic"));
+    }
+    let version = r.unpack::<u32>()?;
+    if version >= 7500 {
+        return Err(Error::new(ErrorKind::InvalidData, "Unsuported FBX version"));
+    }
+    return decode_node_list(r, u64::MAX);
+}
 
 #[cfg(test)]
 mod tests;
--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,14 @@
 use std::io::{Read, Result, Error, ErrorKind, Seek, SeekFrom};
 use std::iter::repeat;
 use std::mem::{forget, size_of, zeroed};
-use std::u64;
 
 use bytepack::{LEUnpacker, Packed};
 
 use flate2::{Decompress, Flush};
 
+/// Represent a typed property of the FBX file format.
 #[derive(Clone)]
-pub enum Record {
+pub enum Property {
     B(bool),
     I16(i16),
     I32(i32),
@@ -19,20 +19,25 @@ pub enum Record {
     String(String),
     BArray(Vec<bool>),
     I8Array(Vec<i8>),
-    I16Array(Vec<i16>),
     I32Array(Vec<i32>),
     I64Array(Vec<i64>),
     F32Array(Vec<f32>),
     F64Array(Vec<f64>),
 }
 
+/// Represent a Node of the FBX file format. Each node has a `name` (or id) and is composed of a 
+/// list of [`Property`](enum.Property.html) and a list of sub [`Node`](struct.Node.html).
 #[derive(Clone)]
 pub struct Node {
-    name: String,
-    properties: Vec<Record>,
-    subnodes: Vec<Node>,
+    /// Name of the node.
+    pub name: String,
+    /// List of properties of the node.
+    pub properties: Vec<Property>,
+    /// List of sub-nodes.
+    pub subnodes: Vec<Node>,
 }
 
+/// Decode a `Property::RawArray`.
 fn decode_raw_array<R: Read>(r : &mut R) -> Result<Vec<u8>> {
     let length = r.unpack::<u32>()? as usize;
     let mut array : Vec<u8> = repeat(0u8).take(length).collect();
@@ -40,13 +45,15 @@ fn decode_raw_array<R: Read>(r : &mut R) -> Result<Vec<u8>> {
     return Ok(array);
 }
 
+/// Decode a `Property::String`.
 fn decode_string<R: Read>(r : &mut R) -> Result<String> {
     match String::from_utf8(decode_raw_array(r)?) {
         Ok(s) => Ok(s),
-        Err(_) => Err(Error::new(ErrorKind::InvalidData, "Invalid UTF-8 Characters in String"))
+        Err(_) => Err(Error::new(ErrorKind::InvalidData, "Invalid UTF-8 characters in string"))
     }
 }
 
+/// Decode a `Property::*Array`.
 fn decode_array<R: Read, T: Packed + Clone>(r : &mut R) -> Result<Vec<T>> {
     let length = r.unpack::<u32>()? as usize;
     let encoding = r.unpack::<u32>()?;
@@ -64,43 +71,45 @@ fn decode_array<R: Read, T: Packed + Clone>(r : &mut R) -> Result<Vec<T>> {
 
         r.read_exact(&mut compressed)?;
         if let Err(_) = deflater.decompress(&compressed, &mut decompressed, Flush::Finish) {
-            return Err(Error::new(ErrorKind::InvalidData, "Failed to Deflate Array"));
+            return Err(Error::new(ErrorKind::InvalidData, "Failed to deflate array"));
         }
 
         // Safe because we made sure the length and capacity of decompressed is length * size_of::<T>() 
         // and we properly forget decompressed
         decompressed.shrink_to_fit();
         unsafe {
-            let mut converted = Vec::<T>::from_raw_parts(decompressed.as_mut_ptr() as *mut T, length, length);
+            let converted = Vec::<T>::from_raw_parts(decompressed.as_mut_ptr() as *mut T, length, length);
             forget(decompressed);
             return Ok(converted);
         }
     }
     else {
-        return Err(Error::new(ErrorKind::InvalidData, "Unknown Array Encoding"));
+        return Err(Error::new(ErrorKind::InvalidData, "Unknown array encoding"));
     }
 }
 
-pub fn decode_record<R: Read>(r: &mut R) -> Result<Record> {
+/// Decode a [`Property`](enum.Property.html).
+pub fn decode_property<R: Read>(r: &mut R) -> Result<Property> {
     match r.unpack()? {
-        b'C' => Ok(Record::B(r.unpack::<u8>()? == 1)),
-        b'Y' => Ok(Record::I16(r.unpack()?)),
-        b'I' => Ok(Record::I32(r.unpack()?)),
-        b'L' => Ok(Record::I64(r.unpack()?)),
-        b'F' => Ok(Record::F32(r.unpack()?)),
-        b'D' => Ok(Record::F64(r.unpack()?)),
-        b'R' => Ok(Record::RawArray(decode_raw_array(r)?)),
-        b'S' => Ok(Record::String(decode_string(r)?)),
-        b'b' =>  Ok(Record::BArray(decode_array::<R, bool>(r)?)),
-        b'c' =>  Ok(Record::I8Array(decode_array::<R, i8>(r)?)),
-        b'i' =>  Ok(Record::I32Array(decode_array::<R, i32>(r)?)),
-        b'l' =>  Ok(Record::I64Array(decode_array::<R, i64>(r)?)),
-        b'f' =>  Ok(Record::F32Array(decode_array::<R, f32>(r)?)),
-        b'd' =>  Ok(Record::F64Array(decode_array::<R, f64>(r)?)),
-        _ => Err(Error::new(ErrorKind::InvalidData, "Invalid Record Type Marker"))
+        b'C' => Ok(Property::B(r.unpack::<u8>()? == 1)),
+        b'Y' => Ok(Property::I16(r.unpack()?)),
+        b'I' => Ok(Property::I32(r.unpack()?)),
+        b'L' => Ok(Property::I64(r.unpack()?)),
+        b'F' => Ok(Property::F32(r.unpack()?)),
+        b'D' => Ok(Property::F64(r.unpack()?)),
+        b'R' => Ok(Property::RawArray(decode_raw_array(r)?)),
+        b'S' => Ok(Property::String(decode_string(r)?)),
+        b'b' =>  Ok(Property::BArray(decode_array::<R, bool>(r)?)),
+        b'c' =>  Ok(Property::I8Array(decode_array::<R, i8>(r)?)),
+        b'i' =>  Ok(Property::I32Array(decode_array::<R, i32>(r)?)),
+        b'l' =>  Ok(Property::I64Array(decode_array::<R, i64>(r)?)),
+        b'f' =>  Ok(Property::F32Array(decode_array::<R, f32>(r)?)),
+        b'd' =>  Ok(Property::F64Array(decode_array::<R, f64>(r)?)),
+        _ => Err(Error::new(ErrorKind::InvalidData, "Invalid property type marker"))
     }
 }
 
+/// Decode a [`Node`](struct.Node.html).
 pub fn decode_node<R: Read + Seek>(r: &mut R) -> Result<Option<Node>> {
     let end_offset = r.unpack::<u32>()? as u64;
     let property_number = r.unpack::<u32>()? as usize;
@@ -118,13 +127,13 @@ pub fn decode_node<R: Read + Seek>(r: &mut R) -> Result<Option<Node>> {
         Ok(s) => s,
         Err(_) => {
             println!("Name Buffer: {:?}", name_buffer);
-            return Err(Error::new(ErrorKind::InvalidData, "Invalid UTF-8 Characters in Node Name"))
+            return Err(Error::new(ErrorKind::InvalidData, "Invalid UTF-8 characters in node name"))
         }
     };
 
-    let mut properties = Vec::<Record>::with_capacity(property_number);
+    let mut properties = Vec::<Property>::with_capacity(property_number);
     for _ in 0..property_number {
-        properties.push(decode_record(r)?);
+        properties.push(decode_property(r)?);
     }
 
     return Ok(Some(Node {
@@ -134,6 +143,7 @@ pub fn decode_node<R: Read + Seek>(r: &mut R) -> Result<Option<Node>> {
     }));
 }
 
+/// Decode a list of [`Node`](struct.Node.html).
 pub fn decode_node_list<R: Read + Seek>(r: &mut R, end : u64) -> Result<Vec<Node>> {
     let mut nodes = Vec::<Node>::new();
     while let Some(node) = decode_node(r)? {
@@ -144,14 +154,4 @@ pub fn decode_node_list<R: Read + Seek>(r: &mut R, end : u64) -> Result<Vec<Node
         }
     }
     return Ok(nodes);
-}
-
-pub fn decode_fbx<R: Read + Seek>(r: &mut R) -> Result<Vec<Node>> {
-    let mut header = [0u8; 23];
-    r.read_exact(&mut header[..])?;
-    if &header != b"Kaydara FBX Binary  \x00\x1a\x00" {
-        return Err(Error::new(ErrorKind::InvalidData, "Invalid FBX Header Magic"));
-    }
-    let version = r.unpack::<u32>()?;
-    return decode_node_list(r, u64::MAX);
 }
